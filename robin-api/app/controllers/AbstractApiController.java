59c2db8869d797d0930188b8ba16450c5b1dfa42
thunderbird84 <robin.github@gmail.com>
2017-05-05 08:52:36 +0700

    init play app

---
+++ robin-api/app/controllers/AbstractApiController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by tuanvu on 5/4/17.
+ */
+public class AbstractApiController extends AbstractController {
+}
---
+++ robin-api/app/controllers/AbstractController.java
@@ -0,0 +1,9 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created by tuanvu on 5/4/17.
+ */
+public class AbstractController extends Controller {
+}
