45ceb2c997099856fc6f6a230ae391042370ea93
thunderbird84 <robin.github@gmail.com>
2017-08-13 13:02:58 +0700

    support qpid docker

---
+++ qpid/bin/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+exec bin/qpid-java \
+    -cp $(cat bin/classpath) \
+    -XX:+UseG1GC \
+    -Xms32m \
+    -Xmx128m \
+    -Duser.timezone="Europe/Stockholm" \
+    -Dfile.encoding="UTF-8" \
+    -Duser.language=en \
+    -Duser.country=US \
+    -Djava.io.tmpdir=/tmp \
+    -Denv=$ENV \
+    -DPNAME=QPBRK \
+    -XX:+HeapDumpOnOutOfMemoryError -Xmx2g \
+    -DQPID_HOME=/app \
+    -DQPID_WORK=/app \
+    -Damqj.logging.level=info \
+    -Damqj.read_write_pool_size=32 \
+    -XX:+ExitOnOutOfMemoryError \
+    -XX:+PrintCommandLineFlags \
+    -XX:+PrintGC \
+    -XX:+PrintGCTimeStamps \
+    -Dcom.sun.management.jmxremote.port=9999 \
+    -Dcom.sun.management.jmxremote.authenticate=false \
+    -Dcom.sun.management.jmxremote.ssl=false \
+    -Dcom.sun.management.jmxremote.rmi.port=9999 \
+    -Djava.rmi.server.hostname=$(/app/bin/resolve-ip.sh) \
+    -DQPID_LOG_APPEND= \
+     org.apache.qpid.server.Main
+
+
+
